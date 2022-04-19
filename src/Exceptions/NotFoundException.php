789e0819a26ced7a92d637438bcbada09a23a332
Daddypro21 <sndemapro@gmail.com>
2022-04-19 13:27:56 +0100

    Ajout du dossier Exceptions et du fichier NotFoundException

---
+++ src/Exceptions/NotFoundException.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Throwable;
+use Core\Controller;
+
+class NotFoundException extends Exception 
+{
+    public function __construct($message = "",$code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message,$code,$previous);
+
+    }
+    
+    public function error404()
+    {
+        \http_response_code(404);
+        $controllerView = new Controller();
+        return $controllerView->view('Default/errors/error404',["title" => "error404"]);
+    }
+}
