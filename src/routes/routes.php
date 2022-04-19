dbd0da5e3e4ad12335c0eddc09f0fdf518ce8090
Daddypro21 <sndemapro@gmail.com>
2022-04-19 18:02:34 +0100

    Ajout des nouvelles routes

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -6,3 +6,10 @@ Route::get('/','App\Controllers\BlogController@index');
 Route::get('/posts','App\Controllers\BlogController@showAll');
 Route::get('/posts/{id}','App\Controllers\BlogController@show');
 Route::post('/create','App\Controllers\BlogController@create');
+
+// Administration
+
+Route::get('/admin/posts','App\Controllers\admin\PostController@index');
+Route::post('/admin/posts/delete/{id}','App\Controllers\admin\PostController@destroy');
+Route::get('/admin/posts/edit/{id}','App\Controllers\admin\PostController@edit');
+Route::post('/admin/posts/update/{id}','App\Controllers\admin\PostController@update');
