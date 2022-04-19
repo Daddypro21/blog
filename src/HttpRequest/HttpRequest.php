9c6190c871e92b431b9227ec2de74cce37df628c
Daddypro21 <sndemapro@gmail.com>
2022-04-19 18:00:05 +0100

    mis en commentaire pour l'instant

--- src/HttpRequest/HttpRequest.php
+++ src/HttpRequest/HttpRequest.php
@@ -3,12 +3,12 @@ namespace App\HttpRequest;
 
 class HttpRequest
 {
-    public function all()
-    {
-        return $_POST;
-    }
-    public function name(string $field)
-    {
-        return $_POST[$field];
-    }
+    // public function all()
+    // {
+    //     return $_POST;
+    // }
+    // public function name(string $field)
+    // {
+    //     return $_POST[$field];
+    // }
 }
