91fca1cb941dad93de2c339696f66bcf638ac020
Daddypro21 <sndemapro@gmail.com>
2022-04-19 18:01:52 +0100

    ajout des nouvelles méthodes, destroy et update

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -8,19 +8,44 @@ class Model
 {
    
     protected $db;
+    protected $table;
+
+    public function __construct($table)
+    {
+        $this->table = $table;
+        $this->db = new DBConnection();
+    }
     
     public function all()
     {
-        $this->db = new DBConnection();
-        $req = $this->db->getPDO()->query("SELECT * FROM posts ORDER BY created_at DESC ");          
+        
+        $req = $this->db->getPDO()->query("SELECT * FROM {$this->table} ORDER BY created_at DESC ");          
         return $req->fetchAll();
     }
 
-    public function findById($id)
+    public function findById(int $id)
     {
-        $db = new DBConnection();
-        $req = $db->getPDO()->prepare("SELECT * FROM posts WHERE id=? "); 
+        
+        $req = $this->db->getPDO()->prepare("SELECT * FROM {$this->table} WHERE id=? "); 
         $req->execute([$id]);        
         return $req->fetchAll();
+    }
+
+    public function destroy(int $id)
+    {
+
+        $req = $this->db->getPDO()->prepare("DELETE FROM {$this->table} WHERE id = ?");
+        $req->execute([$id]);
+        return $req;
+    }
+
+    public function update(int $id, array $data)
+    {
+
+        $data["id"] = $id;
+    
+        $req = $this->db->getPDO()->prepare(" UPDATE {$this->table} SET title = :title,content = :content WHERE id = :id ");
+        $req->execute($data);
+        return $req;
     }
 }
