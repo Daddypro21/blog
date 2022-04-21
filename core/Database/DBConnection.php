6235bf5f9690f28c45b040e9a4f9b58b8b5baede
Daddypro21 <sndemapro@gmail.com>
2022-04-21 12:46:01 +0100

    Deplacement du dossier Database dans le dossier core

---
+++ core/Database/DBConnection.php
@@ -0,0 +1,25 @@
+<?php
+namespace Core\Database;
+
+use PDO;
+
+class DBConnection
+{
+    private $dbname = "Blog";
+    private $host ="localhost";
+    private $username = "root";
+    private $password = "";
+    private $pdo;
+
+    public function __construct()
+    {
+       
+    }
+
+    public function getPDO():PDO 
+    {
+        return $this->pdo ?? $this->pdo = new PDO("mysql:dbname={$this->dbname};host={$this->host}",
+        $this->username,$this->password,[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
+        
+    }
+}
