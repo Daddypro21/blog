7e34b229591945baaf8d8a2b66e3e9a509be3895
Daddypro21 <sndemapro@gmail.com>
2022-05-10 17:03:55 +0100

    modification de session_start

--- public/index.php
+++ public/index.php
@@ -1,15 +1,13 @@
 <?php
-session_start();
+use Core\SessionStart;
 use Core\Route;
 use App\Exceptions\NotFoundException;
 
 require "../vendor/autoload.php";
 
-require "../src/routes/routes.php";
+(new SessionStart())->sessionStart(0,'/','localhost',true,true);
 
-// Route::get('/','App\Controllers\HomeController@index');
-// Route::get('/home/show/{id}','App\Controllers\HomeController@show');
-// Route::post('/home/create','App\Controllers\HomeController@create');
+require "../src/routes/routes.php";
 
 try{
     Route::run();
